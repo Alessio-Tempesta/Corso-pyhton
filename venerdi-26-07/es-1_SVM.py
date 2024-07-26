cbbef7e819f12ab833f6454d66bd11dc15f2472e
Alessio-Tempesta <aletem999@gmail.com>
2024-07-26 16:21:02 +0200

    modifiche

--- venerdi-26-07/es-1_SVM.py
+++ venerdi-26-07/es-1_SVM.py
@@ -20,7 +20,7 @@ X = X[y != 1, :2]
 y = y[y != 1]
 
 # Divisione del dataset in training set e test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 # Visualizzazione del dataset
 _, ax = plt.subplots()
